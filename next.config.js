789ddabfba5082aa7a767cecad31e7d09b4fce79
Jithen_g <simi.saju@colehaan.com>
2025-01-25 12:50:54 +0530

    Fix WebSocket dependencies

--- next.config.js
+++ next.config.js
@@ -16,6 +16,16 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        bufferutil: false,
+        'utf-8-validate': false,
+      }
+    }
+    return config
+  },
 };
 
 module.exports = nextConfig;
